2d3a38a7cf68a742e191695b614e3a26eb798cf2
hendrik <g9a2hvk9@me.com>
2017-05-17 12:22:08 +0100

    Adds beginning of session functionality but sessions cannot be retrieved yet

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const session = require('client-sessions');
 
 const app = express();
 
 const db = require('./server/models')
 
+
+app.use(session({
+  cookieName: 'currentUser',
+  secret: 'very_secret_super_secret',
+}));
 app.use(logger('dev'));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 const User = db.User;
 const Listing = db.Listing;
@@ -21,7 +26,8 @@ app.get('/test', function(req, res) {
   User.findAll()
     .then(function (User) {
       res.json(User);
-    });
+    })
+    .then(console.log(req.currentUser.user.name))
 });
 
 app.post('/user/new', function(req, res) {
@@ -31,6 +37,7 @@ app.post('/user/new', function(req, res) {
     email: req.body.email,
     password: req.body.password,
   })
+  .then(req.currentUser.user = User.findOne({where: {username: req.body.username}}))
   .then(res.redirect('/test'))
 });
 
