e3ac7cf8e98b69e6134ea68984cef5979bad0022
hendrik <g9a2hvk9@me.com>
2017-05-18 16:47:03 +0100

    Fully encrypts passwords and authenticates users

--- app.js
+++ app.js
@@ -6,23 +6,33 @@ const path = require('path');
 const app = express();
 const db = require('./server/models')
 const bcrypt = require('bcrypt-nodejs')
-const salt = bcrypt.genSaltSync(10);
-
+// const salt = bcrypt.genSaltSync(10);
 
 app.use(session({
   cookieName: 'session',
   secret: 'very_secret_super_secret',
 }));
-
 app.use(logger('dev'));
-app.set('views', __dirname + '/views');
-app.engine('html', require('ejs').renderFile);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
 const User = db.User;
 const Listing = db.Listing;
+
+app.get('/welcome', function(req, res) {
+  if (req.session.user) {
+    var currentUser = req.session.user
+    res.render(path.resolve('views/welcome.html'), {
+      username: currentUser.username,
+      name: currentUser.name,
+    });
+  } else {
+    res.redirect('/signup')
+  };
+});
 
 app.get('/test', function(req, res) {
   User.findAll()
@@ -33,6 +43,7 @@ app.get('/test', function(req, res) {
 
 app.post('/user/new', function(req, res) {
   var pass = req.body.password;
+  var salt = bcrypt.genSaltSync(10);
   var hash = bcrypt.hashSync(pass, salt)
   User.create({
     name: req.body.name,
@@ -47,27 +58,8 @@ app.post('/user/new', function(req, res) {
   }, 500))
 });
 
-app.get('/welcome', function(req, res) {
-  if (req.session.user) {
-    var currentUser = req.session.user
-    res.render(path.resolve('views/home.html'), {
-      username: currentUser.username,
-      name: currentUser.name,
-    });
-  } else {
-    res.redirect('/signup')
-  };
-});
-
 app.get('/signup', function(req, res) {
   res.render(path.resolve('views/index.html'));
-});
-
-app.get('/listings', function(req, res) {
-  Listing.findAll()
-    .then(function (Listing) {
-      res.json(Listing);
-    });
 });
 
 app.get('/logout', function(req, res) {
@@ -80,16 +72,33 @@ app.get('/login', function(req, res) {
 })
 
 app.post('/user/login', function(req, res) {
-  User.find({ where: { username: req.body.username,  password: bcrypt.hashSync(req.body.password, salt) }}).then(function(user) {
-      if (!user) {
-        res.redirect('/signup')
+  var pass = req.body.password;
+  var user = User.find({ where: { username: req.body.username}}).then(function(user) {
+    var result = bcrypt.compareSync(pass, user.password);
+      if (result == true){
+        req.session.user = user
+        res.redirect('/welcome');
       } else {
-        req.session.user = user;
-        res.redirect('/welcome')
-      }
-    }).catch(function(err) {
-      console.log('Error')
+        res.redirect('/signup');
+      };
+  });
+});
+
+app.get('/listings', function(req, res) {
+  Listing.findAll()
+    .then(function (Listing) {
+      res.json(Listing);
     });
+});
+
+app.get('/listings/new', function(req, res) {
+  res.render(path.resolve('views/listings.html'));
+});
+
+app.get('/listings/create', function(req, res) {
+  Listing.create({
+    content: "beautiful little cottage, b*tch"
+  })
 });
 
 require('./server/routes')(app);
