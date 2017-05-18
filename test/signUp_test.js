eb9476adbeb36ef30e35c56a66f908afec13e966
Sami <sami@Samis-MacBook-Pro.local>
2017-05-18 10:28:49 +0100

    saved testing

---
+++ test/signUp_test.js
@@ -0,0 +1,12 @@
+var request = require("request");
+var assert = require("assert");
+var app = require("../app.js");
+var signup_url = "http://localhost:8000/signup"
+
+describe("Testing Sign Up", function(){
+  it("allows us to fill in form and presents with welcome message")
+request.get(signup_url, function(error, response, body){
+
+
+});
+});
